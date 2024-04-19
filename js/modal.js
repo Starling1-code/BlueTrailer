d46064970287c02ee970d6209de01f4433b8c575
Vantaelisk <camilanova12@gmail.com>
2024-04-18 22:07:11 -0400

    Agregando cuadro modal de los táilers

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,15 @@ document.getElementById('listaMovie').addEventListener('click', function() {
     }
   });
   
+  // LÓGICA PARA EL MODAL DEL TRAILER
+
+  document.getElementById('openTrailer').addEventListener('click', function() {
+    document.getElementById('modal_trailer').style.display = 'flex';
+    document.getElementById('modal').style.display = 'none';
+  });
+  
+  window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('modal_trailer')) {
+      document.getElementById('modal_trailer').style.display = 'none';
+    }
+  });
